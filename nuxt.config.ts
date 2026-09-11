cdb63e72f6f17dfb65a4a46db34a1b4173728422
SkyNeonMC <79129672+SkyNeonMC@users.noreply.github.com>
2026-09-11 12:34:32 +0800

    fix: set favicon to avatar.png

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,6 +12,9 @@ export default defineNuxtConfig({
       meta: [
         { charset: 'utf-8' },
         { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+      ],
+      link: [
+        { rel: 'icon', type: 'image/png', href: '/avatar.png' }
       ]
     }
   },
